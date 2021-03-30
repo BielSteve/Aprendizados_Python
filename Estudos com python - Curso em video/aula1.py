4aff2ce4045967792c63840ccbf6abab11aa6ca6
Gabriel do Nascimento Pires <gabriel.pires@ndd.tech>
2021-03-30 16:29:55 -0300

    aula 1

---
+++ Estudos com python - Curso em video/aula1.py
@@ -0,0 +1,25 @@
+print('Olá mundo')
+print(7+4)
+print('Ola', 9)
+
+# nome = 'biel'
+# idade = 25
+# peso = 74.8
+
+#Aqui estamos criando as variaveis, e adicionando mensagem na tela atraves do input e guardando o que foi digitado na variavel
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Quanto você pesa??')
+
+print(nome, idade, peso) 
+
+nome1 = input('Qual é o seu nome?')
+print("Olá " + nome1 + "! Prazer em te conhecer!")
+
+
+dia = input('que dia você nasceu?')
+mes = input('que mes você nasceu?')
+ano = input('que ano você nasceu?')
+
+print('Você nasceu no dia ',dia, ' de ', mes, ' de ', ano, ' . Correto ?' )
+
